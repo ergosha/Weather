3ad815500cf3ec5be1d2bdda096575ecb9f78593
Miikka Tyvelä <miikka.tyvela@gmail.com>
2023-10-16 16:24:58 +0300

    done

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Position from './components/Position';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style = {styles.heading}>Current weather!</Text>
+      <Position/>
     </View>
   );
 }
@@ -16,5 +15,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'lightblue'
   },
+  heading : {
+    fontSize: 32,
+    fontWeight: 'bold'
+  }
 });
